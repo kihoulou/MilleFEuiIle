1f3631189adbe7831398414885978a9c85c30dd6
kihoulou <kihoulou@karel.troja.mff.cuni.cz>
2025-08-05 16:41:40 +0200

    Plotting scripts + updated shell scripts

--- run_simple.sh
+++ run_simple.sh
@@ -1,8 +1,8 @@
 # Number of cores
-n_cores=8
+n_cores=4
 
 # Parameters file
-param_file="m_parameters_tectonics_VEP.py"
+param_file="m_parameters_convection.py"
 cp $param_file m_parameters.py
 
 mpirun -n $n_cores python main.py
--- send_docs.sh
+++ send_docs.sh
@@ -1,5 +1,16 @@
-cd docs
+python clear_comments.py
+rm docs/code/*
+cp clean/* docs/code/
+
+# Plotting
+cp data_convection/anim/*.gif docs/gif/
+cp plot_*.py docs/scripts/
+
+cd docs/code
+zip MilleFEuiIle *.py
+cd ..
+
 make clean
 make html
-cd ..
-# cp -r docs /nfs1/WWW/users/kihoulou/mf 
+cp -r _build/html/* /nfs1/WWW/users/kihoulou/millefeuille
+cp code/MilleFEuiIle.zip /nfs1/WWW/users/kihoulou/millefeuille
